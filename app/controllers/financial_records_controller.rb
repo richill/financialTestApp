0c4e716a78d318a764e01ecfe66216db76d83212
richill <richill.tamakloe@gmail.com>
2024-08-07 16:34:52 +0000

    Create financial record controller

---
+++ app/controllers/financial_records_controller.rb
@@ -0,0 +1,10 @@
+class FinancialRecordsController < ApplicationController
+  def index
+  end
+
+  def import
+  end
+
+  def export
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'financial_records/index'
+  get 'financial_records/import'
+  get 'financial_records/export'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
