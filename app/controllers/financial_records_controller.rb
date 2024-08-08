b0dc76c86220bd4119868f79b819546562c6e1cb
richill <richill.tamakloe@gmail.com>
2024-08-08 12:24:13 +0000

    Update controller with import and export actions

--- app/controllers/financial_records_controller.rb
+++ app/controllers/financial_records_controller.rb
@@ -1,10 +1,29 @@
 class FinancialRecordsController < ApplicationController
   def index
+    @financial_records = FinancialRecord.all
   end
 
   def import
+    file = params[:file]
+    return redirect_to root_url, alert: "Please upload a valid Excel file." if file.blank?
+
+    spreadsheet = Roo::Spreadsheet.open(file.path)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      FinancialRecord.create!(row)
+    end
+    redirect_to root_url, notice: "Financial records imported."
   end
 
   def export
+    @financial_records = FinancialRecord.all
+    respond_to do |format|
+      format.xlsx {
+        response.headers[
+          'Content-Disposition'
+        ] = "attachment; filename=financial_records.xlsx"
+      }
+    end
   end
 end
