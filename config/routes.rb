6922fb667689be7a0e99fb3afd55b5e88b702127
richill <richill.tamakloe@gmail.com>
2024-08-08 13:44:01 +0000

    Update upload fail route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'financial_records#index'
-  get  '/financial-records/upload-fail',                            to: 'financial_records#upload_fail'
+  get  '/upload-fail',                            to: 'financial_records#upload_fail'
   resources :financial_records, only: [:index] do
     collection { post :import }
     collection { get :export }
