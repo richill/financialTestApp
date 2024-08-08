9eead52d225a5c019cd50f46ad2af3dfb05e5c9f
richill <richill.tamakloe@gmail.com>
2024-08-08 13:52:07 +0000

    Update upload fail page

--- app/controllers/financial_records_controller.rb
+++ app/controllers/financial_records_controller.rb
@@ -6,20 +6,17 @@ class FinancialRecordsController < ApplicationController
   def import
     file = params[:file]
     if file.blank?
-      redirect_to financial_records_upload_fail_path
+      # binding.pry
+      redirect_to upload_fail_path
     else
-      redirect_to root_url
+      spreadsheet = Roo::Spreadsheet.open(file.path)
+      header = spreadsheet.row(1)
+      (2..spreadsheet.last_row).each do |i|
+        row = Hash[[header, spreadsheet.row(i)].transpose]
+        FinancialRecord.create!(row)
+      end
+      redirect_to root_url, notice: "Financial records imported."
     end
-
-    # return redirect_to root_url, alert: "Please upload a valid Excel file." if file.blank?
-
-    spreadsheet = Roo::Spreadsheet.open(file.path)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      FinancialRecord.create!(row)
-    end
-    redirect_to root_url, notice: "Financial records imported."
   end
 
   def export
